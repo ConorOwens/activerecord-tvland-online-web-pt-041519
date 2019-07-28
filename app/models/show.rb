a18edeae6b25717b8dd9b5c38f1a29bc23c9b2f7
Conor Owens <conorj.owens@gmail.com>
2019-07-28 08:22:00 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,10 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   
+  def actors_list
+    arr = []
+    self.actors.each {|actor| arr << actor.name}
+    arr
+  end
+  
 end
