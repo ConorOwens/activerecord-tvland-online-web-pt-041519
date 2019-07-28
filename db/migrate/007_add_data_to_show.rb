7099f44bdfa03df233f5fc6f414f3486c9d4f4ac
Conor Owens <conorj.owens@gmail.com>
2019-07-28 08:31:58 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_data_to_show.rb
+++ db/migrate/007_add_data_to_show.rb
@@ -1,5 +1,7 @@
 class AddDataToShow < ActiveRecord::Migration[4.2]
   def change
     add_column :show, :day, :string 
+    add_column :show, :genre, :string 
+    add_column :show, :season, :string 
   end
 end
