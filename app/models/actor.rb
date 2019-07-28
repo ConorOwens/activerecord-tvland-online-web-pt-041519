350c4bdc782689a8055e88f0937515ee5ed688f3
Conor Owens <conorj.owens@gmail.com>
2019-07-28 08:17:03 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,7 +1,7 @@
 class Actor < ActiveRecord::Base
   
-  has_many characters
-  
+  has_many :characters
+  has_many :shows, though: :characters
   
   def full_name
     self.first_name + self.last_name
