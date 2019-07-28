6889aab6958dc91ab490eea94a4d57d2ed8416b8
Conor Owens <conorj.owens@gmail.com>
2019-07-28 08:14:21 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
   
+  def full_name
+    self.first_name + self.last_name
+  end
+  
+  def list_roles
+    arr = [] 
+    self.characters.each {|character| arr << character.name + character.}
+    arr
+  end
+  
 end
